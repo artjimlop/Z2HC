3da292a8e70752e585a6908de9e5c23854d33497
Arturo <jimenezlopezarturo@gmail.com>
2026-04-22 17:57:38 +0200

    :poo:

--- src/kv.c
+++ src/kv.c
@@ -1,20 +1,79 @@
 #include <kv.h>
 #include <stdlib.h>
+#include <string.h>
+#define TOMBSTONE 0x1
 
-kv_t *kv_init(size_t capacity) {
-    if (capacity == 0) return NULL;
+size_t hash(char *val, int capacity)
+{
+    size_t hash = 0x13371337deadbeef;
+    while (*val)
+    {
+        hash ^= *val;
+        hash = hash << 8;
+        hash += *val;
+        val++;
+    }
+    return hash % capacity;
+}
+
+int kv_put(kv_t *db, char *key, char *value)
+{
+    if (!db || !key || !value)
+        return -1;
+    size_t idx = hash(key, db->capacity);
+    for (int i = 0; i < db->capacity - 1; i++)
+    {
+        size_t real_idx = (idx + 1) % db->capacity;
+        kv_entry_t *entry = &db->entries[idx];
+
+        if (entry->key && entry->key != (void*) TOMBSTONE && !strcmp(entry->key, key))
+        {
+            char *newval = strdup(value);
+            if (!newval)
+                return -1;
+            entry->value = newval;
+            return real_idx;
+        }
+
+        if (!entry->key || entry->key == (void*) TOMBSTONE)
+        {
+            char *newval = strdup(value);
+            char *newkey = strdup(key);
+            if (!newval || !newkey)
+            {
+                free(newkey);
+                free(newval);
+                return -1;
+            }
+            if (!newval)
+                return -1;
+            entry->value = newval;
+            entry->key = key;
+            db->count = db->count + 1;
+            return real_idx;
+        }
+    }
+    return -2;
+}
+
+kv_t *kv_init(size_t capacity)
+{
+    if (capacity == 0)
+        return NULL;
 
     kv_t *table = malloc(sizeof(kv_t));
-    if (table == NULL) {
+    if (table == NULL)
+    {
         return NULL;
     }
 
     table->capacity = capacity;
     table->count = 0;
     table->entries = calloc(sizeof(kv_entry_t), capacity);
-    if(table->entries == NULL) {
+    if (table->entries == NULL)
+    {
         return NULL;
     }
-    
+
     return table;
 }
--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <kv.h>
 
-int main() {
+int main()
+{
     kv_t *table = kv_init(3);
-    printf("%p", table);
+    printf("%p\n", table);
+    kv_put(table, "hehe", "haha");
+    kv_put(table, "hehe", "hoehoe");
+    kv_put(table, "wewe", "hoehoe");
+
+    for (int i = 0; i < table->capacity; i++)
+    {
+        if (table->entries[i].key)
+        {
+            printf("%s: %s \n", table->entries[i].key, table->entries[i].value);
+        }
+    }
 }
